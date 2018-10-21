624727aa96013ce6a4d79f4914b456710332f98b
doctordp <dominicdemeterca@gmail.com>
2018-10-21 21:55:54 +0200

    insertar frases, me toca los huevos el div que va cambiando, le iré echando un vistazo

---
+++ mini_juegos/yonunca_domi/script.js
@@ -0,0 +1,10 @@
+
+/*document.addEventListener("onload", function Start(){
+    document.getElementById("#uno").innerHTML="Seguro que quieres jugar?"
+}*/
+
+document.addEventListener("onload", Start());
+
+function Start(){
+    document.getElementById("uno").innerHTML="Aquí iría el yo nunca, pero me está tocando las pelotas que no es ni medio normal";
+}
